7376b9b6646127122180d7866375749200bc7bae
Martin Bruse <zondolfin@gmail.com>
2018-03-14 09:17:04 +0100

    Added a ghost castle.

--- rooms.go
+++ rooms.go
@@ -9,10 +9,25 @@ func init() {
 				"Solen tittar fram mellan träden ovanför dig.",
 				"Bakom dig finns en klippa med en mörk grottöppning.",
 				"Framför dig fortsätter stigen djupare in i skogen.",
+				"Du ser en grind och höga torn bakom den.",
 			}
 		},
 	)
 	defaultRoom = start
+	ghostCastle := makeRoom(
+		"Spökslottet",
+		func(s *state) []string {
+			desc := []string{
+				"Under de höga tornen ser du höga fönster och ett par stora dörrar som vaktas av riddarstatyer.",
+				"När du närmar dig så ser du hur statyerna flyttar sig så att de står framför dörren.",
+				"Mellan riddarstatyerna ser du en nyckel i ett hål.",
+			}
+			if s.s.Values["rockPos"] == "hole" {
+				desc = append(desc, "I hålet ligger också en liten sten.")
+			}
+			return desc
+		},
+	)
 	cave := makeRoom(
 		"Grottan",
 		func(s *state) []string {
@@ -33,10 +48,34 @@ func init() {
 		},
 	)
 	start.exits = func(s *state) []*room {
-		return []*room{cave}
+		return []*room{cave, ghostCastle}
 	}
 	cave.exits = func(s *state) []*room {
 		return []*room{start}
+	}
+	smallRock := makeThing("En liten sten")
+	smallRock.actions = func(s *state) map[string]func(*state) {
+		if s.s.Values["location"] == ghostCastle.title && s.held()[smallRock.name] {
+			return map[string]func(*state){
+				"Lägg stenen i hålet mellan riddarna": func(s *state) {
+					s.s.Values["rockPos"] = "hole"
+					delete(s.held(), smallRock.name)
+				},
+			}
+		} else {
+			return map[string]func(*state){}
+		}
+	}
+	ghostCastle.actions = func(s *state) map[string]func(*state) {
+		if !s.held()[smallRock.name] && s.s.Values["rockPos"] != "hole" {
+			return map[string]func(*state){
+				"Plocka upp en sten från marken": func(s *state) {
+					s.held()[smallRock.name] = true
+				},
+			}
+		} else {
+			return map[string]func(*state){}
+		}
 	}
 
 	lightOn := makeThing("Tänd ficklampa")
